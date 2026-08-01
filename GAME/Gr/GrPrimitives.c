443e9b635cb7de9d207d32b888c5482ba3cb63f5
Murad Jabiyev <jabievmurad@gmail.com>
2026-08-01 19:04:28 +0400

    Add files via upload

--- GAME/Gr/GrPrimitives.c
+++ GAME/Gr/GrPrimitives.c
@@ -1,5 +1,6 @@
 #include "../Types.h"
 #include "../Math.h"
+#include "Gr.h"
 volatile U8* VideoBuf=(volatile U8*)0xA0000;
 U0 PutPixel(U32 x,U32 y,U8 color)
 {
@@ -14,5 +15,15 @@ U0 DrawCircle(U32 x,U32 y,U32 r,U8 color)
 		X=Cos(Theta)*r+x;
 		Y=Sin(Theta)*r+y;
 		PutPixel(X,Y,color);
+	}
+}
+U0 DrawSquare(U32 x,U32 y,U32 l,U8 color)
+{
+	GrCoordinates* NewCoordinates;
+	while(l){
+		PutPixel(x++,y,color);
+		NewCoordinates=Mat2x2Rot(x,y,90);
+		PutPixel(NewCoordinates->x,NewCoordinates->y,color);
+		l--;
 	}
 }
